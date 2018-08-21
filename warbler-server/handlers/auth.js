9f40d41bd321aae94c579d797dfbe819a40e302c
Freniel Zabala <frenielzabala@Freniels-MacBook-Pro.local>
2018-08-20 20:19:09 -0700

    added signin functionality

--- warbler-server/handlers/auth.js
+++ warbler-server/handlers/auth.js
@@ -1,8 +1,43 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-exports.signin = function(){
-
+exports.signin = async function(req, res, next){
+    //finding a user
+    try{
+        let user = await db.User.findOne({
+        email: req.body.email 
+        });
+        let { id, username, profileImageUrl} = user;
+        //checking if their password matches what was sent to the server
+        let isMatch = await user.comparePassword(req.body.password);
+        if(isMatch) {
+            let token = jwt.sign({
+                id: id,
+                username: username,
+                profileImageUrl: profileImageUrl
+            },
+            process.env.SECRET_KEY
+        );
+        return res.status(200).json({
+            id: id,
+            username: username,
+            profileImageUrl: profileImageUrl,
+            token: token
+        })
+        } else {
+            return next({
+                status: 400,
+                message: "Invalid Email/Password."
+            });
+        }
+    } catch(err){
+        return next({
+            status: 400,
+            message: "Invalid Email/Password."
+        });
+    }
+    //if it al matches 
+    //log them in
 }
 
 exports.signup = async function(req, res, next){
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -3,7 +3,6 @@ mongoose.set("debug", true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/wrabler", {
     keepAlive: true,
-    useMongoClient: true
 });
 
 module.exports.User = require("./user")
